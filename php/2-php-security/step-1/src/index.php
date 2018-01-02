e2ee1eeb4db8c4ca1e6bed2f9471e24c06296db9
Jeckel <julien.mercier@bureauxapartager.com>
2018-01-02 16:15:13 +0100

    Upgraded demo

--- php/2-php-security/step-1/src/index.php
+++ php/2-php-security/step-1/src/index.php
@@ -1,33 +1,96 @@
 <?php
 namespace Tutorial;
 include dirname(__DIR__) . "/vendor/autoload.php";
-
-use Illuminate\Database\Capsule\Manager;
-
-// Setting up Eloquent database connection
-
-printf("Setting up database connection: ");
+include __DIR__ . "/tools.php";
 
 $settings = include 'settings.php';
-$capsule = new Manager();
-$capsule->addConnection($settings['db']);
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
 
-printf("Done\n");
+connectDatabase($settings['db']);
 
-printf("Running demo script\n");
 
-printf("Step 1: Create a new user: ");
-$user = new User();
-$user->login = "bob";
-$user->passwd = "morane";
-$user->email = "bob.morane@test.fr";
-$user->save();
-printf("Done\n");
+/**
+ * Create new user
+ */
+function createNewUser()
+{
+    printTitle("Enter user details:");
+    $user = new User();
+    $user->login = readUserEntry(" - Login: ");
+    $user->passwd = readUserEntry(" - Password: ");
+    $user->email = readUserEntry(" - Email: ");
+    try {
+        $user->save();
+    } catch(\Illuminate\Database\QueryException $e) {
+        if (! $e->getCode() == 23000) {
+            throw $e;
+        }
+        // Duplicate key
+        printFailure('User already exists');
+        return false;
+    }
+    printSuccess();
+    return true;
+}
 
-printf("List users: \n");
 
-foreach(User::All() as $user) {
+/**
+ * List all users
+ */
+function listUsersFromDb()
+{
+    printTitle("List all users from Database:");
+    foreach(User::All() as $user) {
+        debugUser($user);
+    }
+    printSuccess('done');
+}
+
+/**
+ * @param User $user
+ */
+function debugUser(User $user) {
     printf("%s\n", $user);
+}
+
+/**
+ * @return bool
+ */
+function login()
+{
+    printTitle('Try to login with user:');
+    $users = User::where('login', readUserEntry(" - Login: "))
+        ->where('passwd', readUserEntry(" - Password: "))
+        ->get();
+    if (count($users) == 0) {
+        printf(COLOR_RED . "Login failed\n" . COLOR_NC);
+        return false;
+    }
+    printSuccess('User login success:');
+    debugUser($users[0]);
+    printSuccess('done');
+    return true;
+}
+
+
+const MENU_ITEM_ADD_USER = 1;
+const MENU_ITEM_LIST_USERS = 2;
+const MENU_ITEM_LOGIN = 3;
+const MENU_ITEM_EXIT = 9;
+
+$menuItems = [
+    MENU_ITEM_ADD_USER => "Add user",
+    MENU_ITEM_LIST_USERS => "List users",
+    MENU_ITEM_LOGIN => "Try login with a user",
+    MENU_ITEM_EXIT => "Exit"
+];
+
+while(($selectedItem = menu($menuItems)) != MENU_ITEM_EXIT) {
+    switch($selectedItem) {
+        case MENU_ITEM_ADD_USER: while(!createNewUser());
+            break;
+        case MENU_ITEM_LIST_USERS: listUsersFromDb();
+            break;
+        case MENU_ITEM_LOGIN: login();
+            break;
+    }
 }
